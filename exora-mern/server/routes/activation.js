c31d8123114ba7eae159d20d2b8b38d1b0c233df
yeshwanth <yeshwanthsh128@gmail.com>
2025-10-02 18:20:55 +0530

    new

--- exora-mern/server/routes/activation.js
+++ exora-mern/server/routes/activation.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const OAuthService = require('../services/OAuthService');
+const { OAuthService } = require('../services/OAuthService');
 const OAuthTokens = require('../models/OAuthTokens');
 const UserWorkflowInstance = require('../models/UserWorkflowInstance');
 
