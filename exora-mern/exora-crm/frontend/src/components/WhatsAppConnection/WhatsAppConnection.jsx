91cb7815238cf32d5ce788ce1a56998747b23ad3
yeshwanth <yeshwanthsh128@gmail.com>
2025-10-24 22:52:58 +0530

    new

--- exora-mern/exora-crm/frontend/src/components/WhatsAppConnection/WhatsAppConnection.jsx
+++ exora-mern/exora-crm/frontend/src/components/WhatsAppConnection/WhatsAppConnection.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useQuery, useMutation, useQueryClient } from '@tantml:react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import api from '../../services/api';
 import './WhatsAppConnection.css';
 
